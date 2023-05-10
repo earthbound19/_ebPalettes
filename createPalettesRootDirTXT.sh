2a979148dd57f7501c46ace46fd00e43583889c5
Alex Hall <earthbound19@users.noreply.github.com>
2023-05-09 19:23:32 -0600

    - big fix: path in palettesRootDir.txt was incorrect if created by local (./) call of this script. - improve doc

--- createPalettesRootDirTXT.sh
+++ createPalettesRootDirTXT.sh
@@ -1,13 +1,18 @@
 # DESCRIPTION
-# overwrites ~/palettesRootDir.txt with the absolute path to the /palettes directory of this repository. (Creates that file if it does not exist.) The path in that file is read by and used by various scripts in the _ebDev if not other repositories.
+# overwrites ~/palettesRootDir.txt with the absolute path to the /palettes directory of this repository. (Creates that file if it does not exist, and overwrites it if it does exist.) The path in that file is read by and used by various scripts in the _ebDev if not other repositories.
 
 # USAGE
-# With this script in your PATH, run it:
-# ./createPalettesRootDirTXT.sh
+# From the directory with this script, run it locally:
+#    ./createPalettesRootDirTXT.sh
+# Or you may also run this script by letting the system locate and run it if it is in you PATH:
+#    createPalettesRootDirTXT.sh
 
 
 # CODE
 # for calling this script from anywhere if it is in the PATH; get full path to script minus file name:
-fullPathToScript=$0
-palettesRepoBasePath="${fullPathToScript%\/*}"
+# instead of $0 (formerly used), use the following to get the path to this script, which will work for my purposes whether I call this script with ./ or if it's in my path and I call it just by script name; re: https://stackoverflow.com/a/179231 :
+
+palettesRepoBasePath=$(dirname $(readlink -f $0))
 printf "$palettesRepoBasePath"/palettes > ~/palettesRootDir.txt
+
+
